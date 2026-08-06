1f1443e097cbf6bcd3c94deb04d35b317c55b8d1
Benjamin Reis <yasmindosireis@gmail.com>
2026-08-06 10:06:41 -0300

    Create setup.py

---
+++ ros2/sensor_node/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+package_name = "sensor_node"
+
+setup(
+    name=package_name,
+    version="1.0.0",
+    packages=[package_name],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="Robot",
+    maintainer_email="robot@example.com",
+    description="Sensor Node",
+    license="MIT",
+    entry_points={
+        "console_scripts": [
+            "sensor=sensor_node.sensor:main",
+        ],
+    },
+)
