2e5df7462007a1bec7bceaae29a94c8d92942224
Benjamin Reis <yasmindosireis@gmail.com>
2026-08-06 15:44:28 -0300

    Create config.h

---
+++ firmware/src/config.h
@@ -0,0 +1,18 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define WIFI_SSID "SEU_WIFI"
+#define WIFI_PASSWORD "SUA_SENHA"
+
+#define ROBOT_NAME "ANS-Robot-01"
+
+#define PWM_LEFT 25
+#define PWM_RIGHT 26
+
+#define DIR_LEFT 27
+#define DIR_RIGHT 14
+
+#define ENCODER_LEFT 34
+#define ENCODER_RIGHT 35
+
+#endif
