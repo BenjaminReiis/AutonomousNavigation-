bb4a06c41c648dd8882a6fcd414f7147b797cb09
Benjamin Reis <yasmindosireis@gmail.com>
2026-08-06 08:42:57 -0300

    Create Telemetry.tsx

---
+++ frontend/src/components/Telemetry.tsx
@@ -0,0 +1,37 @@
+function Telemetry(){
+
+
+return (
+
+<div className="card">
+
+
+<h2>
+Telemetria
+</h2>
+
+
+<p>
+GPS: ativo
+</p>
+
+
+<p>
+LiDAR: ativo
+</p>
+
+
+<p>
+Câmera: ativa
+</p>
+
+
+</div>
+
+);
+
+
+}
+
+
+export default Telemetry;
