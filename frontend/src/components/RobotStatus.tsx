c586e2d91d46b984f848c177cac8fba5ac7c452a
Benjamin Reis <yasmindosireis@gmail.com>
2026-08-06 08:42:17 -0300

    Create RobotStatus.tsx

---
+++ frontend/src/components/RobotStatus.tsx
@@ -0,0 +1,32 @@
+function RobotStatus(){
+
+
+return (
+
+<div className="card">
+
+
+<h2>
+Status do Robô
+</h2>
+
+
+<p>
+Online
+</p>
+
+
+<p>
+Bateria: 100%
+</p>
+
+
+</div>
+
+);
+
+
+}
+
+
+export default RobotStatus;
